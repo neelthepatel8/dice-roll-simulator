fb7686eaf68abde0253518e072b576cb5db854c3
neelthepatel <neelthepatel8@gmail.com>
2022-11-09 11:42:24 -0500

    Add ability to play as a full game

--- dice-roll-simulator.py
+++ dice-roll-simulator.py
@@ -4,7 +4,7 @@ from helper import *
 
 def start():
     
-    play_game = input("Just a roll or play a full game? (Roll = r, Game = g) ")
+    game_type = input("Just a roll or play a full game? (Roll = r, Game = g) ")
     
     print("Define the dice type: ")
     num_dice = int(input("How many dices do you want to roll? "))
@@ -15,14 +15,17 @@ def start():
     else:
         same_sides = input("All of the same side? (y/n) ")
     
-    if play_game == 'r':
-
-        dices = check_inputs(same_sides, num_dice)
-        
+    dices = check_inputs(same_sides, num_dice)
+    
+    if game_type == 'r':
         ans = roll_all(dices)
-
         print(f"Your roll is: {ans}")
-        
+    
+    else:
+        end_game = 'n'
+        while end_game != 'y':
+            play_game(dices)
+            end_game = input("End Game? (y/n) ")
         
     
 if __name__ == "__main__":
--- helper.py
+++ helper.py
@@ -9,7 +9,31 @@ def check_inputs(same_sides, num_dice):
         
     else:
         for i in range(num_dice):
-            side = int(input(f"Sides for dice {i}: "))
+            side = int(input(f"Sides for dice {i + 1}: "))
             dices.append(Dice(side))
             
     return dices
+
+def make_player_dict(num_players, dices):
+    players = {}
+    for num in range(num_players):
+        name = input(f"Player {num + 1} name: ")
+        players[num] = {}
+        players[num]["name"] = name
+        players[num]["dice"] = dices[num]
+        
+def play_game(dices):
+    # Ask number of players
+    num_players = int(input("Number of players: "))
+    
+    players = make_player_dict(num_players, dices)
+
+    print("How do you want to roll:")
+    print("1: Roll continously")    
+    print("2: Roll After asking") 
+    choice = int(input("Your choice: "))
+    
+    if choice == 1:
+        rolls = float('inf')
+    else: rolls = 1
+       
