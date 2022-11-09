a33a86b19d2a19859ad51ab70b8d764003b1235f
neelthepatel <neelthepatel8@gmail.com>
2022-11-09 10:59:00 -0500

    Add dice class

---
+++ Dice.py
@@ -0,0 +1,9 @@
+class Dice():
+    def __init__(self, sides):
+        self.sides = sides
+    
+    def get_sides(self):
+        return self.sides
+    
+    def set_sides(self, sides):
+        self.sides = sides
