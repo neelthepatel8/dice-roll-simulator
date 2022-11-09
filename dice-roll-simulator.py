d930e8f6f067644723a8bd46d3dea8b76e14c273
neelthepatel <neelthepatel8@gmail.com>
2022-11-09 11:25:52 -0500

    If it is 1 dice then dont ask for all same sides

--- dice-roll-simulator.py
+++ dice-roll-simulator.py
@@ -2,18 +2,28 @@ from Dice import Dice
 from dice_helper import *
 from helper import *
 
-def main():
+def start():
     
+    play_game = input("Just a roll or play a full game? (Roll = r, Game = g) ")
+    
+    print("Define the dice type: ")
     num_dice = int(input("How many dices do you want to roll? "))
-    same_sides = input("All of the same side? (y/n) ")
     
+    if num_dice == 1:
+        same_sides = 'y'
+        
+    else:
+        same_sides = input("All of the same side? (y/n) ")
+    
+    if play_game == 'r':
 
-    dices = check_inputs(same_sides, num_dice)
-    
-    
-    ans = roll_all(dices)
+        dices = check_inputs(same_sides, num_dice)
+        
+        ans = roll_all(dices)
 
-    print(f"Your roll is: {ans}")
+        print(f"Your roll is: {ans}")
+        
+        
     
 if __name__ == "__main__":
-    main()
+    start()
